ecdf377b1828fde3a31cae50cfd199b4c5fd0ef8
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-04-21 12:24:44 +0200

    feat /downloaded-bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,6 +15,8 @@ defineProps({
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+
+    <h1 class="btn btn-primary">ciao</h1>
   </div>
 </template>
 
