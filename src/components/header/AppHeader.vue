20d56e3fa8d6e693b76e86834deab22ef43dee88
GiacomoMilitello <g.militello95@gmail.com>
2024-04-22 11:44:23 +0200

    structure with sass&components

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header class="d-flex align-items-center gap-3 p-2 px-5">
+    <h1 class="fs-2">App Header</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../../styles/partials/variables" as *;
+@use "../../styles/partials/mixins" as *;
+
+</style>
