bc65d2fb68aafdb203b90f8ab608f13d6524adf0
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-04-22 15:50:08 +0200

    feat /add-store.js

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    loginUrl: 'http://127.0.0.1:8000/login',
+    registerUrl: 'http://127.0.0.1:8000/register',
+    apiUrl: 'http://127.0.0.1:8000',
+})
+
