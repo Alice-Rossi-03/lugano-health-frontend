38336f93b8ed01e1b23c939ea72f0a1221bf4853
GiacomoMilitello <g.militello95@gmail.com>
2024-04-23 17:07:24 +0200

    structure review main

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,43 @@
+<script>
+
+import { store } from '@/store';
+
+export default {
+    name: "Footer",
+    data() {
+        return {
+            store
+        }
+    }
+};
+
+</script>
+
+<template>
+
+<div class="d-green-bg p-5">
+    <div class="container border rounded-3 p-4">
+        <div>
+           <h5 class="text-white">SERVIZI</h5>
+           <ul>
+                <li>link</li>
+                <li>link2</li>
+                <li>link3</li>
+           </ul>
+        </div>
+
+    </div>
+</div>
+   
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use "../styles/partials/variables" as *;
+@use '../styles/general.scss';
+
+
+h2 {
+    color: $d-green;
+}
+</style>
