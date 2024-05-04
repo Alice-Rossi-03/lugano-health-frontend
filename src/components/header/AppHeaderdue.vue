8d51fb648aad2723c22383e273d6f9d6608e088f
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-05-04 12:35:47 +0200

    sistemato header

--- src/components/header/AppHeaderdue.vue
+++ src/components/header/AppHeaderdue.vue
@@ -13,24 +13,28 @@ export default {
 
 <template>
 
-  
-  
+
+
   <header class="px-5 py-2 pt-1 d-green-bg">
-    <nav id="top" class="rounded-4 px-5 mt-4 navbar navbar-expand-lg bg-body-tertiary">
+    <nav id="top" class="rounded-4 px-5 mt-4 navbar navbar-expand-lg bg-body-tertiary border-0">
+      
       <div class="container-fluid">
         <a class="lugano-health" href="#">Lugano Health</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div class="d-flex justify-content-end gap-3">
+          <div class="d-flex ms-auto gap-3"> 
             <a class="btn btn-success" :href="`${store.registerUrl}`">REGISTRATI COME MEDICO</a>
             <a class="btn btn-success" :href="`${store.loginUrl}`">ACCEDI ALL'AREA MEDICI</a>
           </div>
         </div>
       </div>
     </nav>
-    
+
+
+
   </header>
 </template>
 
@@ -39,15 +43,15 @@ export default {
 @use "../styles/partials/variables" as *;
 @use '../styles/general.scss';
 
-#top{
+#top {
   background: linear-gradient(90deg, #233d29, #ffe600);
 
 }
+
 .lugano-health {
-  font-size: 2.5em; 
+  font-size: 2.5em;
   font-family: 'Bebas Neue', sans-serif;
   text-decoration: none;
   color: $s-yellow;
 }
-
 </style>
