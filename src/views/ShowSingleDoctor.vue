8f0cd54512cdd45d0d88dc3553a8069f25cfc427
MyUsername <figusgiacomo04@gmail.com>
2024-05-06 19:10:14 +0200

    feat: api stars

--- src/views/ShowSingleDoctor.vue
+++ src/views/ShowSingleDoctor.vue
@@ -15,6 +15,7 @@ export default {
             doctor: [],
             rating: 0,
             hoverRating: 0,
+            
 
             name: '', //Messaggi
             email: '', //Messaggi
@@ -25,6 +26,7 @@ export default {
             comment: '', //Reviews
 
             success: false,
+            reviewSuccess: false,
             errors: {},
             loading: false
         }
@@ -42,7 +44,21 @@ export default {
             })
         },
         setRating(rating) {
-            this.rating = rating;
+            this.rating = rating
+
+            const data = {
+                doctor_id: this.doctor[0]?.id, 
+                vote_id: rating 
+            }
+
+            axios.post('http://127.0.0.1:8000/api/votes', data).then(res => {
+                this.reviewSuccess = res.data.success
+
+                if (!this.reviewSuccess) {
+                    this.errors = res.data.errors
+                } 
+
+            })
         },
         sendMessage() {
             const data = {
@@ -141,10 +157,10 @@ export default {
                     <!-- STARS -->
                     <div>
                         <svg v-for="star in 5" :key="star" @click="setRating(star)" @mouseover="hoverRating = star"
-                            @mouseleave="hoverRating = 0" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 576 512">
-                            <path :fill="star <= (hoverRating || rating) ? 'gold' : 'gray'"
-                                d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                        @mouseleave="hoverRating = 0" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 576 512">
+                        <path :fill="star <= (hoverRating || rating) ? 'gold' : 'gray'"
+                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                         </svg>
                     </div>
 
