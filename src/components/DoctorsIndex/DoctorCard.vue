cc5f3c4d0bca74ee775a7051c9404fe1e88f1111
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-05-07 01:53:57 +0200

    feat /edit-cards

--- src/components/DoctorsIndex/DoctorCard.vue
+++ src/components/DoctorsIndex/DoctorCard.vue
@@ -26,53 +26,87 @@ export default {
 
     <div>
 
-       
-        <RouterLink class="card border border-success text-decoration-none" :to="{ name: 'single-doctor', params:{slug: propsElement.slug }}">
+
+        <RouterLink class="card border border-success text-decoration-none"
+            :to="{ name: 'single-doctor', params: { slug: propsElement.slug } }">
             <!-- <RouterLink class="card pointer-event text-decoration-none" :to="{ name: 'single-doctor'}"> -->
 
             <div class="card-body text-capitalize row">
 
-                <div class="col-6">
-                    
-                    <figure class="col-3 d-flex align-items-center justify-content-center w-75 mb-2">
-                        <img v-if="propsElement.ProfilePic!= null" class="img-fluid img-thumbnail rounded rounded-circle w-100" :src="`${store.apiBase}storage/${propsElement.ProfilePic}`" alt="title">
-                        <img v-else class="img-fluid img-thumbnail rounded rounded-circle w-100" src="../../img/userpicture.jpg" alt="ProfilePicture">
-                    </figure>
+                <div>
 
-                    <!-- <h2><RouterLink :to="{name: 'single-project', params:{slug: propsElement.slug}}" class="card-title text-warning">{{ propsElement.title }}</RouterLink></h2> -->
+                    <div class="d-flex gap-4">
+                        <figure class="col-3 d-flex align-items-center justify-content-center w-50 mb-2">
+                            <img v-if="propsElement.ProfilePic != null"
+                                class="img-fluid img-thumbnail rounded rounded-circle w-100"
+                                :src="`${store.apiBase}storage/${propsElement.ProfilePic}`" alt="title">
+                            <img v-else class="img-fluid img-thumbnail rounded rounded-circle w-100"
+                                src="../../img/userpicture.jpg" alt="ProfilePicture">
+                        </figure>
 
-                    <h4 class="fw-bolder">Dott. {{ propsElement.name }} {{ propsElement.surname }}</h4>
+                        <div>
+                            <h4 class="fw-bolder">Dott. {{ propsElement.name }} {{ propsElement.surname }}</h4>
 
-                    <span class="badge bg-success my-2" v-for="(item, index) in propsElement.specializations"
-                            :key="item.id">
-                            {{ item.name }}
-                        </span>
+                            <div class="mt-2">
+                                <span>{{ propsElement.address }}</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="d-flex gap-4 mt-3">
+                        <div class="w-50">
+                            <span class="badge bg-success my-2" v-for="(item, index) in propsElement.specializations"
+                                :key="item.id">
+                                {{ item.name }}
+                            </span>
+                        </div>
+
+
+                        <div class="col-6" v-if="propsElement.specializations && propsElement.specializations.length">
+
+                            <div v-show="propsElement.performances">
+                                <h5 class="mb-2">Prestazioni:</h5>
+                                <p class="card-text overflow-hidden" style="width: 100px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ propsElement.performances }}</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
 
                     <div>
-                        {{ propsElement.address }}
+
 
                         <!-- STARS -->
                         <div class="mt-2">
-                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
-                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
-                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
-                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
-                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path
+                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
+                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path
+                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
+                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path
+                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
+                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path
+                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
+                            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                <path
+                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                            </svg>
                         </div>
                     </div>
 
                 </div>
 
 
-                <div class="col-6" v-if="propsElement.specializations && propsElement.specializations.length">
 
-
-                    <div v-show="propsElement.performances">
-                        <h5 class="mb-2">Prestazioni:</h5>
-                        <p class="card-text">{{ propsElement.performances }}</p>
-                    </div>
-
-                </div>
 
             </div>
         </RouterLink>
@@ -87,11 +121,12 @@ export default {
 
 .card {
     width: 20em;
-    min-height: 20rem;
-    max-height: 20rem;
+    min-height: 22rem;
+    max-height: 22rem;
     overflow-y: auto;
+    overflow-x: hidden;
 
-    &:hover{
+    &:hover {
         border: 4px solid $l-green;
     }
 
@@ -100,7 +135,8 @@ export default {
             color: $d-green;
         }
     }
-    h4{
+
+    h4 {
         color: $d-green;
     }
 
