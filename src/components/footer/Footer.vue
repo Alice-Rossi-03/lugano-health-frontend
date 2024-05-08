c7c7a71724aac397d48ff1d0a6940318d861090c
GiacomoMilitello <g.militello95@gmail.com>
2024-05-08 16:35:55 +0200

    footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -16,34 +16,108 @@ export default {
 <template>
 
 <div class="d-green-bg p-5">
-    
-    <!-- BEST DOCTORS -->
-    <div class="d-green-bg">
-        <div class="d-flex container justify-content-between align-items-center">
-            <div class="d-flex align-items-center">
-                <svg class="me-5" xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" fill="gold"
-                    viewBox="0 0 384 512">
-                    <path
-                        d="M173.8 5.5c11-7.3 25.4-7.3 36.4 0L228 17.2c6 3.9 13 5.8 20.1 5.4l21.3-1.3c13.2-.8 25.6 6.4 31.5 18.2l9.6 19.1c3.2 6.4 8.4 11.5 14.7 14.7L344.5 83c11.8 5.9 19 18.3 18.2 31.5l-1.3 21.3c-.4 7.1 1.5 14.2 5.4 20.1l11.8 17.8c7.3 11 7.3 25.4 0 36.4L366.8 228c-3.9 6-5.8 13-5.4 20.1l1.3 21.3c.8 13.2-6.4 25.6-18.2 31.5l-19.1 9.6c-6.4 3.2-11.5 8.4-14.7 14.7L301 344.5c-5.9 11.8-18.3 19-31.5 18.2l-21.3-1.3c-7.1-.4-14.2 1.5-20.1 5.4l-17.8 11.8c-11 7.3-25.4 7.3-36.4 0L156 366.8c-6-3.9-13-5.8-20.1-5.4l-21.3 1.3c-13.2 .8-25.6-6.4-31.5-18.2l-9.6-19.1c-3.2-6.4-8.4-11.5-14.7-14.7L39.5 301c-11.8-5.9-19-18.3-18.2-31.5l1.3-21.3c.4-7.1-1.5-14.2-5.4-20.1L5.5 210.2c-7.3-11-7.3-25.4 0-36.4L17.2 156c3.9-6 5.8-13 5.4-20.1l-1.3-21.3c-.8-13.2 6.4-25.6 18.2-31.5l19.1-9.6C65 70.2 70.2 65 73.4 58.6L83 39.5c5.9-11.8 18.3-19 31.5-18.2l21.3 1.3c7.1 .4 14.2-1.5 20.1-5.4L173.8 5.5zM272 192a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM1.3 441.8L44.4 339.3c.2 .1 .3 .2 .4 .4l9.6 19.1c11.7 23.2 36 37.3 62 35.8l21.3-1.3c.2 0 .5 0 .7 .2l17.8 11.8c5.1 3.3 10.5 5.9 16.1 7.7l-37.6 89.3c-2.3 5.5-7.4 9.2-13.3 9.7s-11.6-2.2-14.8-7.2L74.4 455.5l-56.1 8.3c-5.7 .8-11.4-1.5-15-6s-4.3-10.7-2.1-16zm248 60.4L211.7 413c5.6-1.8 11-4.3 16.1-7.7l17.8-11.8c.2-.1 .4-.2 .7-.2l21.3 1.3c26 1.5 50.3-12.6 62-35.8l9.6-19.1c.1-.2 .2-.3 .4-.4l43.2 102.5c2.2 5.3 1.4 11.4-2.1 16s-9.3 6.9-15 6l-56.1-8.3-32.2 49.2c-3.2 5-8.9 7.7-14.8 7.2s-11-4.3-13.3-9.7z" />
-                </svg>
+    <div class="d-flex justify-content-between mb-4">
 
-                <div class="d-flex flex-column justify-content-center">
-                    <h3 class="text-white">I MIGLIORI SPECIALISTI</h3>
-                    <p class="text-white">Selezionati accuratamente <br>
-                        Tutti a portata di click, sul nostro sito
-                    </p>
-                </div>
+        <div class="d-flex flex-column gap-3">
+            <svg class="me-5" xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" fill="gold" viewBox="0 0 384 512">
+                <path d="M173.8 5.5c11-7.3 25.4-7.3 36.4 0L228 17.2c6 3.9 13 5.8 20.1 5.4l21.3-1.3c13.2-.8 25.6 6.4 31.5 18.2l9.6 19.1c3.2 6.4 8.4 11.5 14.7 14.7L344.5 83c11.8 5.9 19 18.3 18.2 31.5l-1.3 21.3c-.4 7.1 1.5 14.2 5.4 20.1l11.8 17.8c7.3 11 7.3 25.4 0 36.4L366.8 228c-3.9 6-5.8 13-5.4 20.1l1.3 21.3c.8 13.2-6.4 25.6-18.2 31.5l-19.1 9.6c-6.4 3.2-11.5 8.4-14.7 14.7L301 344.5c-5.9 11.8-18.3 19-31.5 18.2l-21.3-1.3c-7.1-.4-14.2 1.5-20.1 5.4l-17.8 11.8c-11 7.3-25.4 7.3-36.4 0L156 366.8c-6-3.9-13-5.8-20.1-5.4l-21.3 1.3c-13.2 .8-25.6-6.4-31.5-18.2l-9.6-19.1c-3.2-6.4-8.4-11.5-14.7-14.7L39.5 301c-11.8-5.9-19-18.3-18.2-31.5l1.3-21.3c.4-7.1-1.5-14.2-5.4-20.1L5.5 210.2c-7.3-11-7.3-25.4 0-36.4L17.2 156c3.9-6 5.8-13 5.4-20.1l-1.3-21.3c-.8-13.2 6.4-25.6 18.2-31.5l19.1-9.6C65 70.2 70.2 65 73.4 58.6L83 39.5c5.9-11.8 18.3-19 31.5-18.2l21.3 1.3c7.1 .4 14.2-1.5 20.1-5.4L173.8 5.5zM272 192a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM1.3 441.8L44.4 339.3c.2 .1 .3 .2 .4 .4l9.6 19.1c11.7 23.2 36 37.3 62 35.8l21.3-1.3c.2 0 .5 0 .7 .2l17.8 11.8c5.1 3.3 10.5 5.9 16.1 7.7l-37.6 89.3c-2.3 5.5-7.4 9.2-13.3 9.7s-11.6-2.2-14.8-7.2L74.4 455.5l-56.1 8.3c-5.7 .8-11.4-1.5-15-6s-4.3-10.7-2.1-16zm248 60.4L211.7 413c5.6-1.8 11-4.3 16.1-7.7l17.8-11.8c.2-.1 .4-.2 .7-.2l21.3 1.3c26 1.5 50.3-12.6 62-35.8l9.6-19.1c.1-.2 .2-.3 .4-.4l43.2 102.5c2.2 5.3 1.4 11.4-2.1 16s-9.3 6.9-15 6l-56.1-8.3-32.2 49.2c-3.2 5-8.9 7.7-14.8 7.2s-11-4.3-13.3-9.7z" />
+            </svg>
+    
+            <div class="d-flex flex-column justify-content-center gap-3">
+                <h4 class="">I MIGLIORI SPECIALISTI</h4>
+                <p class="text-white">Selezionati accuratamente <br>
+                    Tutti a portata di click, sul nostro sito
+                </p>
             </div>
-            <div class="d-flex flex-column align-items-end">
-            <h4 class="text-end">
+        </div>
+    
+        <!-- SERVICES -->
+        <div class="d-flex flex-column gap-3">
+            <h4>Servizi</h4>
+    
+            <ul class="list-unstyled">
+                <li><a href="#">Privacy e cookies</a></li>
+                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">Contatti</a></li>
+                <li><a href="#">Lavora con noi</a></li>
+                <li><a href="#">Condizioni di Servizio</a></li>
+                <li><a href="#">Ufficio stampa</a></li>
+                <li><a href="#">In che modo ordiniamo i risultati</a></li>
+            </ul>
+        </div>
+        
+        <!-- FOR USERS -->
+        <div class="d-flex flex-column gap-3">
+            <h4>Per i Pazienti</h4>
+    
+            <ul class="list-unstyled">
+                <li><a href="#">Dottori</a></li>
+                <li><a href="#">Medici di base</a></li>
+                <li><a href="#">Strutture</a></li>
+                <li><a href="#">Chiedi al dottore</a></li>
+                <li><a href="#">Prestazioni</a></li>
+                <li><a href="#">Patologie</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+    
+        <!-- FOR DOCTORS -->
+        <div class="d-flex flex-column gap-3">
+            <h4>Per i professionisti sanitari</h4>
+    
+            <ul class="list-unstyled">
+                <li><a href="#">Prezzi</a></li>
+                <li><a href="#">Area professionisti sanitari</a></li>
+                <li><a href="#">Risorse gratuite</a></li>
+                <li><a href="#">Centro Assistenza per Professionisti</a></li>
+            </ul>
+        </div>
+    
+        <!-- CONTACTS -->
+        <div class="d-flex flex-column align-items-center gap-3">
+            <figure class="d-flex align-items-center" style="width: 80px;">
+                <img class="img-fluid" src="../../img/lugano-health-logo-1.png" alt="">
+              </figure>
+    
+              <ul class="list-unstyled">
+                <li class="d-white-color">Lugano Health S.r.l.</li>
+                <li class="d-white-color">Piazza Riziero Rezzonico</li>
+                <li class="d-white-color">6900 Lugano, Svizzera</li>
+              </ul>
+    
+              <ul class="list-unstyled d-flex">
+    
+                <!-- FACEBOOK -->
+                <li>
+                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg></a>
+                </li>
+    
+                <!-- TWITTER -->
+                <li>
+                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/></svg></a>
+                </li>
+    
+                <!-- INSTAGRAM -->
+                <li>
+                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7 53.3 53.3 0 1 0 -59.3-88.7zm142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9 .1-11.2 .1c-3.3 0-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1 .1 7.9 .1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1c3.3 0 7.2 0 11.4 .1c25.5 .3 64.8 .7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1 .1-11.4c.3-25.5 .7-64.9-6.5-83l0 0c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5A82 82 0 1 1 178.4 324.2a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5l0 0c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM357 389c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z"/></svg></a>
+                </li>
+    
+                <!-- LINKEDIN -->
+                <li>
+                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a>
+                </li>
+              </ul>
+        </div>
+    
+    </div>
+
+    <div class="d-flex flex-column align-items-center">
+            <h4 class="text-center">
                 “Dove c’è amore per l’umanità, <br>
                 c’è anche amore per l’arte della medicina.”
             </h4>
-            <p class="text-white">-Paracelso</p>
-            </div>
+            <p class="text-white mt-2">-Paracelso</p>
         </div>
-    </div>
-
 </div>
    
 </template>
@@ -56,5 +130,21 @@ export default {
 
 h4{
     color: $s-yellow;
+}
+
+a{
+    text-decoration: none;
+    color: $d-white;
+    &:hover{
+        color: $s-yellow;
+    }
+}
+svg{
+    fill: gold;
+    width: 40px;
+    height: 40px;
+    &:hover{
+        fill: $d-white;
+    }
 }
 </style>
