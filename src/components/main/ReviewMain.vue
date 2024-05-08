5d18231a9c0422b505c82c3ea5240dae41404cd0
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-05-08 12:03:25 +0200

    modifica carouselll

--- src/components/main/ReviewMain.vue
+++ src/components/main/ReviewMain.vue
@@ -17,24 +17,25 @@ export default {
 </script>
 
 <template>
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-        <div v-for="doctor in doctors" :key="doctor.id" class="col">
-            <!-- CARD DA CICLARE SUCCESSIVAMENTE -->
-            <div class="card pb-5 pt-4 px-5">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h5>{{ doctor.name }}</h5>
-                    <figure class="" style="width: 100px;">
-                        <img v-if="doctor.ProfilePic != null"
-                            class="img-fluid img-thumbnail rounded rounded-circle w-100"
-                            :src="`${store.apiBase}storage/${doctor.ProfilePic}`" alt="title">
-                        <img v-else class="img-fluid img-thumbnail rounded rounded-circle w-100"
-                            src="../../img/userpicture.jpg" alt="ProfilePicture">
-                    </figure>
-                </div>
-                <p>{{ doctor.performances }}</p>
+
+
+    <div v-for="doctor in doctors" :key="doctor.id" class="col-4" id='card'>
+        <!-- CARD DA CICLARE SUCCESSIVAMENTE -->
+        <div class="card pb-5 pt-4 px-5">
+            <div class="d-flex justify-content-between align-items-center">
+                <h5>{{ doctor.name }}</h5>
+                <figure class="" style="width: 100px;">
+                    <img v-if="doctor.ProfilePic != null" class="img-fluid img-thumbnail rounded rounded-circle w-100"
+                        :src="`${store.apiBase}storage/${doctor.ProfilePic}`" alt="title">
+                    <img v-else class="img-fluid img-thumbnail rounded rounded-circle w-100"
+                        src="../../img/userpicture.jpg" alt="ProfilePicture">
+                </figure>
             </div>
+            <p>{{ doctor.performances }}</p>
         </div>
     </div>
+
+
 </template>
 
 <style lang="scss" scoped>
